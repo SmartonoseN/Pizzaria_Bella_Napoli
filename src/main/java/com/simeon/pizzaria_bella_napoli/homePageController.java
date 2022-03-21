304dd7e1959431dc27bcdef1b53c378d831af595
SmartonoseN <simeon.mihayloff@gmail.com>
2022-03-21 23:54:02 +0100

    Handle CheckBox is working. I have implemented semi working class for price calculation.

--- src/main/java/com/simeon/pizzaria_bella_napoli/homePageController.java
+++ src/main/java/com/simeon/pizzaria_bella_napoli/homePageController.java
@@ -1,8 +1,6 @@
 package com.simeon.pizzaria_bella_napoli;
 
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
-import javafx.collections.ListChangeListener;
+
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
@@ -35,6 +33,7 @@ public class homePageController {
     public TextArea infoBox;
     public Button saveButton;
     public TextArea statusBox;
+    double price = 0.0;
 
     @FXML
     private void initialize() {
@@ -43,6 +42,7 @@ public class homePageController {
     }
 
     @FXML
+    //Programmierung der "Speichern" Knopf
     public void handleSaveBtnClick() {
         saveButton.setOnAction(new EventHandler<ActionEvent>() {
             @Override
@@ -64,22 +64,69 @@ public class homePageController {
                     statusBox.setText(e.toString());
                 }
             }
-
         });
     }
 
     @FXML
+    //Programmierung der In Stadt Checkbox
+    public void handleCheckBoxClick() {
+        inCity.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                while (inCity.isSelected()) {
+                    if (price >= 10.00) {
+                        price = price + 1;
+                    }
+                }
+                while (inCity.isSelected() == false) {
+                    if (price >= 20.00) {
+                        price = price + 2;
+                    }
+                }
+            }
+        });
+    }
+
+    @FXML
+    //Programmierung der Pizza Menu
     public void handlePizzaMenu() throws SQLException {
         pizzaMenu.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent actionEvent) {
                 String pizza = pizzaMenu.getValue().toString();
                 orderedPizza.getItems().add(pizza);
+                calculateOrderSum();
             }
         });
     }
+    //TODO calculate the tax of the price for one pizza and then add it together
+    //TODO convert to 2 numbers after decimal
 
+    //Programmierung der Summe
+    public void calculateOrderSum() {
+        if (orderedPizza.getItems().isEmpty() == false) {
 
+            if (orderedPizza.getItems().size() >= 2) {
+                for(int i = 0; i < orderedPizza.getItems().size(); i++) {
+                    String pizza = orderedPizza.getItems().toString();
+                    String[] sum = pizza.split(" ");
+                    price = Double.parseDouble(sum[1]) + price;
+                }
+                price = price * 1.07;
+                double roundedPrice = Math.round((price*100.00)/100.00);
+                String textPrice = Double.toString(roundedPrice);
+                priceTextField.setText(textPrice);
+            } else {
+                String pizza = orderedPizza.getItems().toString();
+                String[] sum = pizza.split(" ");
+                price = Double.parseDouble(sum[1])*1.07;
+                double roundedPrice = Math.round((price*100.00)/100.00);
+                String textPrice = Double.toString(roundedPrice);
+                priceTextField.setText(textPrice);
+            }
+        }
+    }
+//Programmierung der Verbindung mit Database Pizza Table
     public void getPizza() {
         String url = "jdbc:sqlite:Database/database.db";
         String getPizza = "SELECT * FROM pizza";
@@ -92,17 +139,18 @@ public class homePageController {
             List<String> pizzas = new LinkedList<>();
 
             while (rs.next()) {
-                pizzas.add(rs.getString("name"));
+                pizzas.add(rs.getString("name") + " " + rs.getString("price") + " €");
             }
             for (int i = 0; i < pizzas.size(); i++) {
                 pizzaMenu.getItems().add(pizzas.get(i));
+
             }
         } catch (SQLException e) {
             statusBox.setText(e.toString());
         }
 
     }
-
+//Programmierung der Verbindung mit Database Employee (Mitarbeiter) Table
     public void getEmployee() {
         String url = "jdbc:sqlite:Database/database.db";
         String getEmployee = "SELECT * FROM employee";
@@ -124,5 +172,4 @@ public class homePageController {
             statusBox.setText(e.toString());
         }
     }
-
 }
